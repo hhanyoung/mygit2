36f33bf799c8df700bf03457f017496c6bd08db1
hhanyoung <whgks0621@naver.com>
2020-06-12 00:39:18 +0900

    devC branch

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -4,6 +4,8 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println("a");
+  System.out.println("after devB");
+  System.out.println("after devC");
 	}
 
 }
